371a12630b192539ea9aaa347ad245b5bdfa42c4
Pabasara Samarakoon <pabasara96samarakoon@gmail.com>
2025-04-11 10:13:55 +0530

    Add files via upload

--- rotationobj.cpp
+++ rotationobj.cpp
@@ -6,13 +6,19 @@ float angle = 0;
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
   glPushMatrix();
-  glRotatef(angle, 0.0, 0.0, 1.0);
-  glBegin(GL_POLYGON);
-    glColor3f(1.0, 0.0, 0.0);
+  glRotatef(angle, 1.0, 1.0, 1.0);
+  glBegin(GL_QUADS);
+    glColor3f(0.0, 0.0, 1.0);
     glVertex2f(-0.5, -0.5);
     glVertex2f(-0.5, 0.5);
     glVertex2f(0.5, 0.5);
     glVertex2f(0.5, -0.5);
+  glEnd();
+  glBegin(GL_TRIANGLES);
+    glColor3f(0.0, 1.0, 1.0);
+	glVertex2f(0.4, 0.4);
+    glVertex2f(0.4, -0.4);
+    glVertex2f(-0.4, -0.4);
   glEnd();
   glPopMatrix();
   glutSwapBuffers();
@@ -22,7 +28,9 @@ void animate(int value) {
   angle += 2.0;
   if (angle > 360) angle -= 360;
   glutPostRedisplay();
-  glutTimerFunc(25, animate, value);
+  //glutTimerFunc(25, animate, value);
+   glutTimerFunc(10, animate, value);
+   //glutTimerFunc(100, animate, value);
 }
 
 int main(int argc, char** argv) {
